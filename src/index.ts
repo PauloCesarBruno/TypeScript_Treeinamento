1cd6a399588d4c227c9c283d5a6bcdb865fe287e
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-11-26 17:31:26 -0300

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+function component() {
+    const element = document.getElementById('content');
+
+    element.innerHTML = "<h1>Olá Mundo !</h1>"
+    element.innerHTML += "<h1>Olá Mundo !</h1>"
+    element.innerHTML += "<h1>Olá Mundo !</h1>"
+    element.innerHTML += "<h1>Olá Mundo !</h1>"
+    element.innerHTML += "<h1>Olá Mundo !</h1>"
+    element.innerHTML += "<h1>Olá Mundo !</h1>"
+    
+    return element;
+  }
+
+  component();
