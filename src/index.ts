1a8d5964489b0faabe1963b9b7d314f235aecb8c
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2023-12-29 18:06:25 -0300

    Evolução do código, Inclusão de codigo JS no proj.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,73 @@
+import { accessOptions, userType } from "./Models";
+
+let content  = document.getElementById('content') as HTMLInputElement;
 const button = <HTMLInputElement> document.querySelector('button[id="add"')
 const accessRadio = <HTMLElement> document.getElementById('accessRadio');
 button.addEventListener('click',addEmployee)
 
-enum accessOptions {
-  administrator = "administrador",
-  manager = "gerente",
-  employee = "funcionario"
-}
-
 const accessOptionsValues = Object.values(accessOptions)
 
-accessOptionsValues.forEach((value: string, i: number) => {
+// Chamar Usuários.
+const getUser = async(): Promise <userType[]> => {
+  // Função que seta o servidor criado para meu "mock"
+  const response: Response = await fetch('http://localhost:5011/users');
+  const users: userType[] = await response.json();
+  return users;
+}
+
+// Montar o Layout.
+const updateUserLayout = async (): Promise <void> => {
+  const users: userType[] = await getUser();
+
+  users.map((user: userType)=>{
+    content.innerHTML += <string>(createLine(user)
+      );
+  });
+};
+
+updateUserLayout();
+
+/* Abaixo exemplos de como posso variar um assertion em Type Script, dou 3 Exemplos:
+ou seja um Union atracéz de um pipe-line, um TypeCast e um álias.*/ 
+function addEmployee(): void {
+ // Abaixo Exemplo de um "Union - usando um pipe-line"
+ // let fullName: HTMLInputElement | null = document.querySelector('#fullName') 
+ // Abaixo Exemplo de Assertion ,exemplo 01, tipo um TypeCast:
+ // let register = <HTMLInputElement> document.querySelector('#register')
+ // Abaixo Exemplo de Assertion 02 - usando (HTMLInputElement) como um álias para as variaveis
+ // let admin = document.querySelector('input[name="access"]:checked') as HTMLInputElement ;
+ // let active = document.querySelector('#active') as HTMLInputElement;
+ // let addressHome = document.querySelector('#addressHome') as HTMLInputElement;
+ // let addressWork = document.querySelector('#addressWork') as HTMLInputElement;
+ // let user: userType;
+
+ // TRABALHANDO COM destruturação:
+ let formFields = [
+  <HTMLInputElement>document.querySelector('#fullName'),
+  <HTMLInputElement>document.querySelector('#register'),
+  document.querySelector('input[name="access"]:checked') as HTMLInputElement,
+  document.querySelector('#active') as HTMLInputElement,
+  document.querySelector('#addressHome') as HTMLInputElement,
+  document.querySelector('#addressWork') as HTMLInputElement
+ ]
+
+  const [fullName, register, admin, active, addressHome, addressWork] = formFields
+
+  let user: userType =  {
+    fullName: fullName!.value,
+    register: register.value != '' ? register.value : undefined,
+    active: active.checked,
+    access: <accessOptions>admin.value
+  }
+
+  content.innerHTML += <string>createLine(
+    user,
+    addressHome.value,
+    addressWork.value
+  );  
+  }
+
+  accessOptionsValues.forEach((value: string, i: number) => {
   accessRadio.innerHTML += `
   <div class="form-check">
   <input class="form-check-input" type="radio" name="access" id="accessRadio${i}" value="${value}">
@@ -18,46 +75,36 @@ accessOptionsValues.forEach((value: string, i: number) => {
   ${value}
   </label>
 </div> 
-`
-})
+`;
+});
 
-/* Abaixo exemplos de como posso variar um assertion em Type Script, dou 3 Exemplos:
-ou seja um Union atracéz de um pipe-line, um TypeCast e um álias.*/ 
-function addEmployee(): void {
-  let content  = document.getElementById('content') as HTMLInputElement;
-  
-  // Abaixo Exemplo de um "Union - usando um pipe-line"
-  let fullName: HTMLInputElement | null = document.querySelector('#fullName') 
-  // Abaixo Exemplo de Assertion ,exemplo 01, tipo um TypeCast:
-  let register = <HTMLInputElement> document.querySelector('#register')
-   // Abaixo Exemplo de Assertion 02 - usando (HTMLInputElement) como um álias para as variaveis
-  let admin = document.querySelector('input[name="access"]:checked') as HTMLInputElement ;
-  let active = document.querySelector('#active') as HTMLInputElement;
+(<HTMLInputElement>document.getElementById('accessRadio0')).checked = true;
 
-  content.innerHTML += <string>(
-
-  createLine(fullName!.value, register.value, admin.value, active.checked)
-  );
-}
-
-function createLine(
-  fullName: string,
-  nrRegister: number | string, // outro Ex. de Union..
-  admin: string,
-  active: boolean
-): string {
-    return `
+function createLine({
+  fullName,
+  register = Math.random().toString(36).substring(7).toUpperCase(),
+  active = false,
+  access = accessOptions.undefined
+}: userType, 
+  ...address: string []): string {
+  return `
   <div class="card mb-1">
     <div class="card-header">
-      ${nrRegister}
+      ${register}
     </div>
     <div class="card-body">
       <h5 class="card-title">${fullName}</h5>
       <a href="#" class="btn btn-primary">${active ? 'Ativo' : 'Inativo'}</a>
     </div>
+    ${
+      address.length > 0 ?
+      `<div class="card-body">
+      <h6 class="card-title">${address.join('<br/>')}</h6>     
+    </div>` : ''    
+    }
+    
     <div class="card-footer bg-transparent border-success">
-    Acesso: ${admin}
+    Acesso: ${access}
     </div>
-  </div>
-    `;
+  </div>`;
 }
